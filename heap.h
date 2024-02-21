2e16fc14d64a6deacba14c30c8b47413ef57b99f
Aarushi Garg <aarushigarg@gmail.com>
2024-02-20 18:16:06 -0800

    heap

--- heap.h
+++ heap.h
@@ -2,8 +2,9 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
-template <typename T, typename PComparator = std::less<T> >
+template <typename T, typename PComparator = std::less<T>>
 class Heap
 {
 public:
@@ -61,14 +62,83 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
+  int ary;
+  PComparator comp;
+  std::vector<T> vec;
+  
+  void heapifyUp() {
+    int i = size()-1;  // last node
 
+    while (i > 0) {
+      int parI = (i-1)/ary;  // parent of i
 
+      if (comp(vec[i], vec[parI])) 
+        std::swap(vec[i], vec[parI]);  // if i is better than its parent, swap
 
+      else  // parent is better so i cannot move up anymore
+        break;
+
+      i = parI;
+    }
+  }
+
+  void heapifyDown() {
+    int i = 0;  // root
+    while (true) {
+      int firstChildI = ary*i + 1;  // +1 bc we index from 0
+      if (firstChildI >= size())   // if there are no children
+        break;
+
+      int bestChildI = firstChildI;
+      for (int x = 2; x < ary+1; x++) {  // loop through all children and find the best
+        int y = ary*i + x;
+
+        if (y >= size())  // no more children
+          break;
+
+        if (comp(vec[y], vec[bestChildI]))  // compare current child with best
+          bestChildI = y;
+      }
+
+      if (comp(vec[bestChildI], vec[i]))  //if best child is better than parent, swap
+        std::swap(vec[bestChildI], vec[i]);
+        
+      else 
+        break;
+
+      i = bestChildI;
+    }
+  }
 
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T,PComparator>::Heap(int m, PComparator c) {
+  ary = m;
+  comp = c;
+}
 
+template <typename T, typename PComparator>
+Heap<T,PComparator>::~Heap() {
+
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item) {
+  vec.push_back(item);
+  heapifyUp();
+}
+
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const {
+  return vec.size() == 0;
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const {
+  return vec.size();
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -81,14 +151,11 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("Empty heap");
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
-
-
+  return vec[0];
 }
 
 
@@ -101,12 +168,12 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("Empty heap");
   }
-
-
-
+  // swap first and last
+  std::swap(vec[0], vec[size()-1]);
+  vec.pop_back();
+  heapifyDown();
 }
 
 
