d2c55d5d31963a51c58aad3c4b5c6f502a5980c6
Aarushi Garg <aarushigarg@gmail.com>
2024-02-27 09:45:34 -0800

    stack updated for std

--- stack.h
+++ stack.h
@@ -6,31 +6,31 @@
 
 // Use inheritance from std::vector (choose public/private) as appropriate
 template <typename T>
-class Stack : private vector<T>
+class Stack : private std::vector<T>
 {
 public:
     Stack() {}
     ~Stack() {}
     bool empty() const {
-        return vector<T>::empty();
+        return std::vector<T>::empty();
     }
     size_t size() const {
-        return vector<T>::size();
+        return std::vector<T>::size();
     }
     void push(const T& item) {
-        vector<T>::push_back(item);
+        std::vector<T>::push_back(item);
     }
     void pop() { // throws std::underflow_error if empty
-        if (vector<T>::empty()) {
-            throw underflow_error("Stack is empty");
+        if (std::vector<T>::empty()) {
+            throw std::underflow_error("Stack is empty");
         }
-        vector<T>::pop_back();
+        std::vector<T>::pop_back();
     }
     const T& top() const { // throws std::underflow_error if empty
-        if (vector<T>::empty()) {
-            throw underflow_error("Stack is empty");
+        if (std::vector<T>::empty()) {
+            throw std::underflow_error("Stack is empty");
         }
-        return vector<T>::back();
+        return std::vector<T>::back();
     }
     // Add other members only if necessary
 };
