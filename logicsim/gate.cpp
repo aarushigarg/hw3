c6b39cb2fd5823c4b387c0a54cf56a7d2f84dca4
Aarushi Garg <aarushigarg@gmail.com>
2024-02-27 17:05:24 -0800

    not

--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -26,17 +26,26 @@ NotGate::NotGate(Wire* a, Wire* o) : Gate(1, o) {
 }
 
 Event* NotGate::update(uint64_t current_time) {
-	char state = 'X';
-	char in = m_inputs[0]->getState();
-	if (in == '0') {
-		state = '1';
+	char state = '0';
+    Event* e = NULL;
+
+	for(Wire* w : m_inputs) {
+		char in = w->getState();
+
+		if(in == '1') {
+			state = '1';
+			break;
+		}
+		else if(in == 'X')
+			state = 'X';
 	}
-	else if (in == '1') {
-		state = '0';
+	if (state != m_current_state) {
+		m_current_state = state;
+		uint64_t next = current_time + m_delay;
+		e = new Event {next,m_output,state};
 	}
-	
-	Event* e = new Event{ current_time + m_delay, m_output, state };
-	return e;
+			
+  return e;
 }
 
 And2Gate::And2Gate(Wire* a, Wire* b, Wire* o) : Gate(2,o)
